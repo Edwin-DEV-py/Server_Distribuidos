efaca3e954e1d4fd6ce95ef4d6a368cfb6c72bf7
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2024-03-19 06:44:33 -0500

    Update views.py

--- userGRPC/views.py
+++ userGRPC/views.py
@@ -3,7 +3,7 @@ from .serializers import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from grpc import insecure_channel
-
+import requests
 
 class AuthenticationView(APIView):
     
@@ -14,3 +14,15 @@ class AuthenticationView(APIView):
             
             username = serializers.validated_data.get('username')
             password = serializers.validated_data.get('password')
+
+            url = 'http://10.152.164.94:8000/api/token/'
+            data = {
+                "email":username,
+                "password":password
+            }
+
+            response = requests.post(url, json = data)
+
+            print(response.text)
+
+            return Response(response.json())

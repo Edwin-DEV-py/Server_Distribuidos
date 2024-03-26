77b8334f9f772dc568b92426490982b84b6d4ac5
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2024-03-26 03:22:21 -0500

    archivos grpc

--- userGRPC/serializers.py
+++ userGRPC/serializers.py
@@ -3,3 +3,10 @@ from rest_framework import serializers
 class UserCredentialsSerializer(serializers.Serializer):
     username = serializers.CharField()
     password = serializers.CharField()
+
+class UserDataSerializer(serializers.Serializer):
+    username = serializers.CharField()
+    name = serializers.CharField()
+    email = serializers.EmailField()
+    phone = serializers.IntegerField()
+    password = serializers.CharField()
--- userGRPC/views.py
+++ userGRPC/views.py
@@ -3,8 +3,13 @@ from .serializers import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from grpc import insecure_channel
+from .. import server_pb2 as grpc_pb2
+from .. import server_pb2_grpc as grpc_pb2_grpc
 import requests
+import grpc
 
+
+#autenticacion por grpc
 class AuthenticationView(APIView):
     
     def post(self, request):
@@ -14,15 +19,57 @@ class AuthenticationView(APIView):
             
             username = serializers.validated_data.get('username')
             password = serializers.validated_data.get('password')
+            
+            #conexion gRPC con el server de usuarios
+            
+            channel = grpc.insecure_channel('127.0.0.1:8000')
+            stub = grpc_pb2_grpc.AuthenticationServiceStub(channel)
 
-            url = 'http://10.152.164.94:8000/api/token/'
+            #crear el mensjae
+            credentials = grpc_pb2.UserCredentials(username=username,password=password)
+            
+            #llamar al metodo
+            authentication = stub.AuthenticateUser(credentials)
+            
+            #procesar la respuesta del servidor
+            if authentication.success:
+                return Response({'token': authentication.token})
+            
+            else:
+                return Response({'error_message': authentication.error_message})
+            
+        else:
+            return Response(serializers.errors, status=400)
+        
+class RegisterViewGRPC(APIView):
+    
+    def post(self, request):
+        
+        serializers = UserDataSerializer(data=request.data)
+        
+        if serializers.is_valid():
+            username = serializers.validated_data.get('username')
+            name = serializers.validated_data.get('name')
+            email = serializers.validated_data.get('email')
+            phone = serializers.validated_data.get('phone')
+            password = serializers.validated_data.get('password')
+            
+            url = 'http://10.152.164.94:8000/api/register/'
             data = {
-                "email":username,
+                "username":username,
+                "name":name,
+                "email":email,
+                "phone":phone,
                 "password":password
             }
-
+            
             response = requests.post(url, json = data)
 
             print(response.text)
 
             return Response(response.json())
+
+        return Response(serializers.errors)
+            
+
+            

4589f03e3d75133151579b6534e1aa7a5a55aeb9
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2024-04-09 02:46:02 -0500

    mysql

--- Server/settings.py
+++ Server/settings.py
@@ -91,6 +91,7 @@ WSGI_APPLICATION = 'Server.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
@@ -98,6 +99,21 @@ DATABASES = {
     }
 }
 
+"""
+
+#MYSQL
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'server',
+        'USER': 'root',
+        'PASSWORD': 'root',
+        'HOST': '127.0.0.1',
+        'PORT': '3306',
+    }
+}
+"""
 
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators

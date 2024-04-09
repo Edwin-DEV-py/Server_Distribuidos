02146d6fc38d0fd5c00b6e28822528597877c419
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2024-04-09 02:32:32 -0500

    DOCKER DE MIERDA

--- userGRPC/views.py
+++ userGRPC/views.py
@@ -2,7 +2,6 @@ from django.shortcuts import render
 from .serializers import *
 from rest_framework.views import APIView
 from rest_framework.response import Response
-from grpc import insecure_channel
 import server_pb2 as grpc_pb2
 import server_pb2_grpc as grpc_pb2_grpc
 import requests

a00d574fbdff82184119ea0192c025a994dee7e8
edwin-tarazona <80715680+Edwin-DEV-py@users.noreply.github.com>
2024-05-02 11:04:23 -0500

    Update views_soap.py

--- FolderApp/views_soap.py
+++ FolderApp/views_soap.py
@@ -177,9 +177,9 @@ class SoapService(ServiceBase):
             return "Token inválido"
         
     @rpc(Unicode, Unicode, Unicode, _returns=Unicode)
-    def shareFolderSoap(ctx, token, folderId, ShareFolderSoap):
+    def shareFolderSoap(ctx, token, folderId, sharedUserId):
         try:
-            response = ShareFolderSoap(token, folderId, ShareFolderSoap)
+            response = ShareFolderSoap(token, folderId, sharedUserId)
             
             if response == 'Carpeta compartida correctamente':
                 return 'Carpeta compartida correctamente'
